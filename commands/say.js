8a9575c37aacdb6826f3b36f019319cf54152d2e
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-08-11 08:53:30 +0700

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,15 +1,16 @@
 const { prefix } = require("../config.json");
 const axios = require("axios")
 
-exports.run = (client, message) => {
-  if(!message.body) client.reply(message.from, `Gunakan: *${prefix}say <message>*\n\n* Tanpa < >`, message.id)
-client.reply(message.from, message.body, message.id)
-
+exports.run = (client, message, args) => {
+  if(!args[0]) { client.reply(message.from, `Gunakan: *${prefix}say <message>*\n\n* Tanpa < >`, message.id) 
+  } else {
+client.reply(message.from, message.body.slice(5), message.id)
+}
 };
 
 exports.help = {
     name: "Say",
     description: "Mengikuti perkataan mu",
-    usage: `${prefix}say (message)`,
+    usage: `${prefix}say <message>`,
     cooldown: 0
 };
