9bfacde8f8b8c069d0f04cec77b7fa9aa3f2c1b6
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2021-08-12 15:29:49 +0700

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -1,28 +1,37 @@
-const { opts } = require("../config.json");
-const prefix = opts.prefix
+const { prefix, stickerPackName } = require("../config.json");
 const { decryptMedia } = require("@open-wa/wa-decrypt");
 const uaOverride = "WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
 
-exports.run = async (client, message) => {
+
+exports.run = async (bot, message, args) => {
     const now = Date.now();
     if (message.isMedia && message.type === "image") {
         const media = await decryptMedia(message, uaOverride);
-        await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, {
+        await bot.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, {
             author: message.sender.pushname,
-            pack: opts.sticker-pack-name
+            pack: stickerPackName
         });
         return console.log(`[DEBUG] Sticker was generated in ${Date.now() - now}ms`);
     } else if (message.quotedMsgObj && message.quotedMsgObj.type === "image") {
         const media = await decryptMedia(message.quotedMsgObj, uaOverride);
-        await client.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, {
+        await bot.sendImageAsSticker(message.from, `data:image/jpeg;base64,${media.toString("base64")}`, {
             author: message.sender.pushname,
-            pack: opts.sticker-pack-name
+            pack: stickerPackName
         });
         return console.log(`[DEBUG] Sticker was generated in ${Date.now() - now}ms`);
-    } catch (error) {
-            return client.reply(message.from, "Error", message.id);
+    } else if ((message.isMedia || message.isGif) || (message.mimetype === "video/mp4" || message.mimetype === "image/gif") || message.type === "video") {
+        if (message.duration >= 10) return bot.reply(message.from, "❎ Sorry, but your attachment size is too large", message.id);
+        const mediaData = await decryptMedia(message, uaOverride);
+        try {
+            await bot.sendMp4AsSticker(message.from, mediaData, {}, {
+                author: message.sender.pushname,
+                pack: "TEST BOT"
+            });
+        } catch (error) {
+            return bot.reply(message.from, "Error", message.id);
         } else {
-        return client.reply(message.from, `❎ Pakai *${prefix}sticker* di caption gambar atau reply gambar yang ingin di jadikan sticker`, message.id);
+          bot.reply(message.from, 'Reply image yang akan di jadikan Sticker!', message.id)
+        }
     }
 };
 
